1377cb5a16634e5fb5c102a5c9758e6b33972f3d
Yusup <awklsgrep@gmail.com>
2021-01-15 23:44:28 +0800

    minor

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker build -t jumpget .
 docker run --name jumpget -p 3100:3100 \
   -p 127.0.0.1:4100:4100 -d \
   -e JUMPGET_PUBLIC_PORT=3100 -e JUMPGET_LOCAL_PORT=4100 \
-  -e JUMPGET_PUBLIC_URL="example.com:3100"  jumpget
+  -e JUMPGET_PUBLIC_URL="http://example.com:3100"  jumpget
