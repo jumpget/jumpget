a9dca4c138b70c37aa6aacfa83423d6c53cd863f
Yusup <awklsgrep@gmail.com>
2021-01-21 16:52:14 +0800

    fix cleaner

--- utils/cleaner.go
+++ utils/cleaner.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -15,6 +17,11 @@ func CleanOldFiles(dir string, dur time.Duration) {
 	for _, info := range fileInfo {
 		if diff := now.Sub(info.ModTime()); diff > dur {
 			log.Printf("Deleting %s which is %s old\n", info.Name(), diff)
+			fullPath := filepath.Join(dir, info.Name())
+			err = os.Remove(fullPath)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 }
